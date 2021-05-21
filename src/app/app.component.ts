818f1a171852454b334b7f11b5e756d0ddd6d666
Nayara <nayara.lopes2007@gmail.com>
2021-05-20 23:18:04 -0300

    Estruturando Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+
+    photos = [
+      {
+        url:'https://static.mundodasmensagens.com/upload/textos/a/-/a-vida-e-feita-de-escolhas-e-a-minha-escolha-e-ser-feliz-38WMj-cxl.jpg',
+        description: 'VIDA'
+      },
+      {
+        url:'https://www.stoodi.com.br/wp-content/uploads/2020/09/fases-da-lua-2.png',
+        description: 'LUA'
+      }
+    ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photo.modules';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
